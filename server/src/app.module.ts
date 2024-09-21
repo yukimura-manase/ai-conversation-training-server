f7ba359fcf38c43e0e34a81ddc4e9d2781ff1430
inoway46 <inoueyuya416@gmail.com>
2024-09-21 17:57:26 +0900

    動くようになった

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { KimerasModule } from './modules/kimeras.module'; // KimerasModuleをインポート
+import { KimerasModule } from './modules/kimeras.module';
+import { AppDataSource } from './data-source';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(), // TypeORMの設定をここに追加
-    KimerasModule, // KimerasModuleをインポート
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10) : undefined,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: AppDataSource.options['synchronize'],
+      logging: AppDataSource.options['logging'],
+      entities: AppDataSource.options['entities'],
+      migrations: AppDataSource.options['migrations'],
+      subscribers: AppDataSource.options['subscribers'],
+    }),
+    KimerasModule,
   ],
 })
 export class AppModule {}
